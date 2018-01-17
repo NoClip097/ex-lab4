e2fb4d67dc48bcaaafe5eeab66c601ebdd26889f
NoClip097 <accelerator097@mail.ru>
2018-01-18 01:25:00 +0300

    Сделана задача 2

--- librip/iterators.py
+++ librip/iterators.py
@@ -1,14 +1,11 @@
 # Итератор для удаления дубликатов
 class Unique(object):
     def __init__(self, items, **kwargs):
-        self.lst=list(items)
-        self.index = 0
+        self.lst=list(items)  # Получаем в список
+        self.index = 0  # Счётчик чтобы бегать по списку и в конце завершить цикл
         self.case_ignore = kwargs.get('ignore_case')
-        self.buf=[]
+        self.buf=[]  # Список для контроля уникальных значений
 
-
-
-        # Нужно реализовать конструктор
         # В качестве ключевого аргумента, конструктор должен принимать bool-параметр ignore_case,
         # в зависимости от значения которого будут считаться одинаковые строки в разном регистре
         # Например: ignore_case = True, Aбв и АБВ разные строки
@@ -16,26 +13,16 @@ class Unique(object):
         # По-умолчанию ignore_case = False
 
     def __next__(self):
-        while self.index <= len(self.lst):
-            if self.index == len(self.lst):
-                raise StopIteration
-            if self.case_ignore:
+        while self.index < len(self.lst):
+            if self.case_ignore:  # Определяемся, будет ли разным регистр
                 i = self.lst[self.index].lower()
             else:
-                i = self.lst[self.index]
-            if i not in self.buf:
-                self.buf.append(i)
-                return i
+                i = self.lst[self.index]  # В одну строку никак не работало
+            if i not in self.buf:  # Проверка на уникальость...
+                self.buf.append(i)  # ...и добавление в случае успеха
+                return i  # ...и возврат в случае успеха
             self.index += 1
         raise StopIteration
-
-            #if i not in self.buf:
-            #    self.buf.append(i)
-            #    return i
-
-
-        #return buf
-        # Нужно реализовать __next__
 
     def __iter__(self):
         return self
