50bc2f674aec70c2f2b0d7e58b807e229daea251
NoClip097 <accelerator097@mail.ru>
2018-01-20 23:19:25 +0300

    Сделаны задачи 3 и 4

--- ex_2.py
+++ ex_2.py
@@ -4,7 +4,7 @@ from librip.iterators import Unique
 
 data1 = [1, 1, 1, 1, 1, 2, 2, 2, 2, 2]
 data2 = gen_random(1, 3, 10)
-data3 = ['Абв','АБВ','абв','аБв']
+data3 = ['a','A','b','B']
 
 # Реализация задания 2
 
